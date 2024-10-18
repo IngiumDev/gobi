2f0407f23806849819ae666abcdfb46fea31b940
Ingiumdev <86780096+IngiumDev@users.noreply.github.com>
2024-10-18 11:05:17 +0200

    Started structs for GTF. Created AnnotationEntry, FrameStarts, Interval, StrandDirection

---
+++ src/Interval.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+
+public class Interval {
+    // one-based, inclusive
+    private int start;
+    private int end;
+
+    public Interval(int end, int start) {
+        this.end = end;
+        this.start = start;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Interval interval = (Interval) o;
+        return start == interval.start && end == interval.end;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start, end);
+    }
+
+    @Override
+    public String toString() {
+        return start+": "+end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+    public int getLength() {
+        return end - start + 1;
+    }
+}
