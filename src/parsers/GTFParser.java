0c0dc9730f800f7f23a2966145b95312491ddbd9
Ingiumdev <86780096+IngiumDev@users.noreply.github.com>
2024-10-20 20:11:41 +0200

    Introduced constants for the column numbers for GTF

--- src/parsers/GTFParser.java
+++ src/parsers/GTFParser.java
@@ -15,13 +15,23 @@ public class GTFParser {
     public final static String[] ATTRIBUTES_TO_ESCALATE_TO_TRANSCRIPT = {"gene_id", "gene_name", "gene_source", "gene_biotype", "transcript_id", "transcript_name", "transcript_source"};
     public final static String[] ATTRIBUTES_TO_ESCALATE_TO_GENE = {"gene_id", "gene_name", "gene_source", "gene_biotype"};
     public final static String[] ATTRIBUTES_TO_ESCALATE_TO_EXON = ATTRIBUTES_TO_ESCALATE_TO_TRANSCRIPT;
+    public static final int FEATURE_COL = 2;
+    public static final int SEQNAME_COL = 0;
+    public static final int SOURCE_COL = 1;
+    public static final int STRAND_COL = 6;
+    public static final int START_COL = 3;
+    public static final int END_COL = 4;
+    public static final int SCORE_COL = 5;
+    public static final int FRAME_COL = 7;
+    public static final int ATTRIBUTE_COL = 8;
+
     // GTF line columns to escalate: seqname, source, strand
     public static GTFAnnotation parseGTF(String gtfFile) {
         GTFAnnotation GTFAnnotation = new GTFAnnotation();
         try (BufferedReader br = new BufferedReader(new FileReader(gtfFile))) {
             String line;
             while ((line = br.readLine()) != null) {
-                if (line.charAt(0) != '#') {
+                if (line.charAt(SEQNAME_COL) != '#') {
                     processGTFLine(line, GTFAnnotation);
                 }
             }
@@ -38,7 +48,7 @@ public class GTFParser {
 
     private static void processGTFLine(String line, GTFAnnotation GTFAnnotation) {
         String[] data = line.split("\t");
-        switch (data[2]) {
+        switch (data[FEATURE_COL]) {
             case "gene" -> processGene(data, GTFAnnotation);
             case "transcript" -> processTranscript(data, GTFAnnotation);
             case "exon" -> processExon(data, GTFAnnotation);
@@ -47,17 +57,17 @@ public class GTFParser {
     }
 
     private static void processCDS(String[] data, GTFAnnotation GTFAnnotation) {
-        Map<String, String> attributes = parseAttributes(data[8]);
+        Map<String, String> attributes = parseAttributes(data[ATTRIBUTE_COL]);
         String geneId = attributes.get("gene_id");
         String transcriptId = attributes.get("transcript_id");
 
-        Gene gene = getOrCreateGene(GTFAnnotation, geneId,data[0], data[1], getStrand(data[6]), attributes);
-        Transcript transcript = getOrCreateTranscript(gene, transcriptId, data[0], data[1], getStrand(data[6]), attributes);
+        Gene gene = getOrCreateGene(GTFAnnotation, geneId,data[SEQNAME_COL], data[SOURCE_COL], getStrand(data[STRAND_COL]), attributes);
+        Transcript transcript = getOrCreateTranscript(gene, transcriptId, data[SEQNAME_COL], data[SOURCE_COL], getStrand(data[STRAND_COL]), attributes);
         int exonNumber = Integer.parseInt(attributes.get("exon_number"));
 
-        Exon exon = getOrCreateExon(exonNumber, transcript, data[0], data[1], getStrand(data[6]), attributes);
+        Exon exon = getOrCreateExon(exonNumber, transcript, data[SEQNAME_COL], data[SOURCE_COL], getStrand(data[STRAND_COL]), attributes);
 
-        CodingSequence cds = new CodingSequence(data[0], data[1], data[2], new Interval(Integer.parseInt(data[3]), Integer.parseInt(data[4])), getScore(data[5]), getStrand(data[6]), getFrame(data[7]), attributes);
+        CodingSequence cds = new CodingSequence(data[SEQNAME_COL], data[SOURCE_COL], data[FEATURE_COL], new Interval(Integer.parseInt(data[START_COL]), Integer.parseInt(data[END_COL])), getScore(data[SCORE_COL]), getStrand(data[STRAND_COL]), getFrame(data[FRAME_COL]), attributes);
         exon.setCds(cds);
         transcript.addCds(cds);
 
@@ -80,19 +90,19 @@ public class GTFParser {
     }
 
     private static void processExon(String[] data, GTFAnnotation GTFAnnotation) {
-        Map<String, String> attributes = parseAttributes(data[8]);
+        Map<String, String> attributes = parseAttributes(data[ATTRIBUTE_COL]);
         String geneId = attributes.get("gene_id");
         String transcriptId = attributes.get("transcript_id");
 
-        Gene gene = getOrCreateGene(GTFAnnotation, geneId,data[0], data[1], getStrand(data[6]), attributes);
-        Transcript transcript = getOrCreateTranscript(gene, transcriptId, data[0], data[1], getStrand(data[6]), attributes);
+        Gene gene = getOrCreateGene(GTFAnnotation, geneId,data[SEQNAME_COL], data[SOURCE_COL], getStrand(data[STRAND_COL]), attributes);
+        Transcript transcript = getOrCreateTranscript(gene, transcriptId, data[SEQNAME_COL], data[SOURCE_COL], getStrand(data[STRAND_COL]), attributes);
 
         int exonNumber = Integer.parseInt(attributes.get("exon_number"));
         Exon exon = transcript.getExonByNumber(exonNumber);
         if (exon == null) {
-            exon = new Exon(data[0], data[1], data[2], new Interval(Integer.parseInt(data[3]), Integer.parseInt(data[4])), getScore(data[5]), getStrand(data[6]), getFrame(data[7]), attributes);
+            exon = new Exon(data[SEQNAME_COL], data[SOURCE_COL], data[FEATURE_COL], new Interval(Integer.parseInt(data[START_COL]), Integer.parseInt(data[END_COL])), getScore(data[SCORE_COL]), getStrand(data[STRAND_COL]), getFrame(data[FRAME_COL]), attributes);
         } else {
-            exon.overwrite(data[0], data[1], data[2], new Interval(Integer.parseInt(data[3]), Integer.parseInt(data[4])), getScore(data[5]), getStrand(data[6]), getFrame(data[7]), attributes);
+            exon.overwrite(data[SEQNAME_COL], data[SOURCE_COL], data[FEATURE_COL], new Interval(Integer.parseInt(data[START_COL]), Integer.parseInt(data[END_COL])), getScore(data[SCORE_COL]), getStrand(data[STRAND_COL]), getFrame(data[FRAME_COL]), attributes);
         }
 
 
@@ -118,19 +128,19 @@ public class GTFParser {
     }
 
     private static void processTranscript(String[] data, GTFAnnotation GTFAnnotation) {
-        Map<String, String> attributes = parseAttributes(data[8]);
+        Map<String, String> attributes = parseAttributes(data[ATTRIBUTE_COL]);
         String geneId = attributes.get("gene_id");
         String transcriptId = attributes.get("transcript_id");
 
-        Gene gene = getOrCreateGene(GTFAnnotation, geneId, data[0], data[1],getStrand(data[6]), attributes);
+        Gene gene = getOrCreateGene(GTFAnnotation, geneId, data[SEQNAME_COL], data[SOURCE_COL],getStrand(data[STRAND_COL]), attributes);
 
         Transcript transcript = gene.getTranscript(transcriptId);
         if (transcript == null) {
-            transcript = new Transcript(data[0], data[1], data[2], new Interval(Integer.parseInt(data[3]), Integer.parseInt(data[4])), getScore(data[5]), getStrand(data[6]), getFrame(data[7]), attributes);
+            transcript = new Transcript(data[SEQNAME_COL], data[SOURCE_COL], data[FEATURE_COL], new Interval(Integer.parseInt(data[START_COL]), Integer.parseInt(data[END_COL])), getScore(data[SCORE_COL]), getStrand(data[STRAND_COL]), getFrame(data[FRAME_COL]), attributes);
             transcript.setId(transcriptId);
             gene.addTranscript(transcriptId, transcript);
         } else {
-            transcript.overwrite(data[0], data[1], data[2], new Interval(Integer.parseInt(data[3]), Integer.parseInt(data[4])), getScore(data[5]), getStrand(data[6]), getFrame(data[7]), attributes);
+            transcript.overwrite(data[SEQNAME_COL], data[SOURCE_COL], data[FEATURE_COL], new Interval(Integer.parseInt(data[START_COL]), Integer.parseInt(data[END_COL])), getScore(data[SCORE_COL]), getStrand(data[STRAND_COL]), getFrame(data[FRAME_COL]), attributes);
         }
 
     }
@@ -152,14 +162,14 @@ public class GTFParser {
     }
 
     private static void processGene(String[] data, GTFAnnotation GTFAnnotation) {
-        Map<String, String> attributes = parseAttributes(data[8]);
+        Map<String, String> attributes = parseAttributes(data[ATTRIBUTE_COL]);
         Gene thisGene = GTFAnnotation.getGenes().get(attributes.get("gene_id"));
         // Check if gene already exists
         if (thisGene != null) {
             // Overwrite the gtf.AnnotationEntry fields with the new one
-            thisGene.overwrite(data[0], data[1], data[2], new Interval(Integer.parseInt(data[3]), Integer.parseInt(data[4])), getScore(data[5]), getStrand(data[6]), getFrame(data[7]), attributes);
+            thisGene.overwrite(data[SEQNAME_COL], data[SOURCE_COL], data[FEATURE_COL], new Interval(Integer.parseInt(data[START_COL]), Integer.parseInt(data[END_COL])), getScore(data[SCORE_COL]), getStrand(data[STRAND_COL]), getFrame(data[FRAME_COL]), attributes);
         } else {
-            Gene gene = new Gene(data[0], data[1], data[2], new Interval(Integer.parseInt(data[3]), Integer.parseInt(data[4])), getScore(data[5]), getStrand(data[6]), getFrame(data[7]), attributes);
+            Gene gene = new Gene(data[SEQNAME_COL], data[SOURCE_COL], data[FEATURE_COL], new Interval(Integer.parseInt(data[START_COL]), Integer.parseInt(data[END_COL])), getScore(data[SCORE_COL]), getStrand(data[STRAND_COL]), getFrame(data[FRAME_COL]), attributes);
             gene.setId(gene.getAttribute("gene_id"));
             GTFAnnotation.addGene(gene);
         }
@@ -168,7 +178,7 @@ public class GTFParser {
 
     public static double getScore(String data) {
         if (Objects.equals(data, ".")) {
-            return 0;
+            return SEQNAME_COL;
         } else {
             return Double.parseDouble(data);
         }
