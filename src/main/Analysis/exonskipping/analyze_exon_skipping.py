176049b84584a26f1e9d045347ee256a7e466076
IngiumDev <86780096+IngiumDev@users.noreply.github.com>
2024-10-25 13:58:22 +0200

    Started Data Analysis script

---
+++ src/main/Analysis/exonskipping/analyze_exon_skipping.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+RESULTS = "/Users/simon/IdeaProjects/gobi/data/exonskipping/outs"
+
+
+if __name__ == '__main__':
+    # List to store the file names
+    tsv_data = {}
+
+    # Iterate over all files in the directory
+    for file_name in os.listdir(RESULTS):
+        data =  pd.readC
+
+    for tsv in tsv_files:
+
