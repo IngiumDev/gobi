55f0c6565722b29b7af3fbef3d5acdf198eb7ae5
InguimDev <86780096+IngiumDev@users.noreply.github.com>
2024-10-25 14:59:46 +0200

    Plotting of box plots

--- src/main/Analysis/exonskipping/analyze_exon_skipping.py
+++ src/main/Analysis/exonskipping/analyze_exon_skipping.py
@@ -4,7 +4,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 RESULTS = "/Users/simon/IdeaProjects/gobi/data/exonskipping/outs"
-
+'''The report should contain
+at least two cumulative plots (see definition on the internet or an R tutorial) showing the
+1Guideline: a bunch of plots without a clear explanation of what is shown, without description of what
+can be observed, or without a statement what can be concluded is not a sufficient report and will be graded
+accordingly.
+3
+distributions of the maximum number of skipped exons and skipped bases, respectively,
+per ES-SE for every GTF file. The plots have to be saved also into your output directory
+named skipped_exons.jpg and skipped_bases.jpg. '''
 
 if __name__ == '__main__':
     # List to store the file names
@@ -12,7 +20,13 @@ if __name__ == '__main__':
 
     # Iterate over all files in the directory
     for file_name in os.listdir(RESULTS):
-        data =  pd.readC
-
-    for tsv in tsv_files:
-
+        # only if .tsv
+        if file_name[-4:] == ".tsv":
+            data = pd.read_csv(file_name, sep="\t")
+            tsv_data[file_name] = data
+    
+    # Create the figures
+    fig, ax = plt.subplots()
+    
+    
+        
