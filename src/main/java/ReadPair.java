36079689b21c3567448ca1354cbe75abe8a88331
IngiumDev <86780096+IngiumDev@users.noreply.github.com>
2024-11-07 16:28:07 +0100

    - ReadSimulatorRunner, wrote method to readcountsfile, to move to other class later - Read, ReadPair implemented logic for creating reads and readpairs

--- src/main/java/ReadPair.java
+++ src/main/java/ReadPair.java
@@ -1,5 +1,28 @@
+import gtf.structs.Interval;
+import gtf.types.StrandDirection;
+
+import java.util.Random;
+import java.util.TreeSet;
+
 public class ReadPair extends Pair<Read, Read> {
     public ReadPair(Read first, Read second) {
         super(first, second);
     }
+
+
+    public ReadPair(String transcriptSequence, int fragmentStart, int fragmentLength, int readLength) {
+        super(new Read(transcriptSequence, StrandDirection.FORWARD, fragmentStart, fragmentLength, readLength),
+                new Read(transcriptSequence, StrandDirection.REVERSE, fragmentStart, fragmentLength, readLength));
+    }
+
+    public void mutateReadPairs(double mutationRate, Random random) {
+        this.getFirst().mutate(mutationRate, random);
+        this.getSecond().mutate(mutationRate, random);
+    }
+
+    public void calculateGenomicPositions(TreeSet<Interval> exonPositions) {
+        this.getFirst().calculateGenomicPositions(exonPositions);
+        this.getSecond().calculateGenomicPositions(exonPositions);
+    }
+
 }
